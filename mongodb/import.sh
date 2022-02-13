9e66c648268259473ac179e51525458546d54bb3
Isira Adithya <rp.isira@gmail.com>
2022-02-13 21:41:01 +0530

    Finished

--- mongodb/import.sh
+++ mongodb/import.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
-mongoimport --db 1337shop --collection users --drop --file /mongodb/users.json --jsonArray;
-mongoimport --db 1337shop --collection prevloginlogs --drop --file /mongodb/prevloginlogs.json --jsonArray;
+#!/bin/bash
+mongoimport --db 1337shop --collection users --drop --file /mongodb/users.json --jsonArray --uri "mongodb://mongo:27017";
+mongoimport --db 1337shop --collection prevloginlogs --drop --file /mongodb/prevloginlogs.json --jsonArray --uri "mongodb://mongo:27017";
